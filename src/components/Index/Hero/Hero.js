ab7d0dcc753746cc636d215cb270a776bd8ff175
Samuel Fialho <samuel@superlativo.pt>
2018-03-26 12:55:37 +0100

    hero style for desktop

--- src/components/Index/Hero/Hero.js
+++ src/components/Index/Hero/Hero.js
@@ -8,7 +8,7 @@ class Hero extends Component {
     render(){
         return(
             <div className="Hero">
-            <h1>Go wild and drop it</h1>
+            <h1>Go wild <br/> and drop it</h1>
             <h2>Welcome to Portugal's <br/> best kept secret</h2>
         </div>
         )
