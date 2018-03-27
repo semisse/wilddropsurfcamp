e9ca65d0626ed0dd0834bff7a2e541dcfa1c6d97
Samuel Fialho <samuel@superlativo.pt>
2018-03-27 13:10:42 +0100

    homepage: social and map

--- src/components/Index/SocialAndMap/Map.js
+++ src/components/Index/SocialAndMap/Map.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Link from 'gatsby-link'
+
+import GoogleMapReact from 'google-map-react'
+
+import Logo from '../../../img/logo.svg'
 
 import './Map.sass'
 
-const Map = () => (
-    <div className="Map">
-        Map
-    </div>
-)
+const AnyReactComponent = ({ text }) => <div className="bubble"><img src={Logo} /></div>
+
+class Map extends Component {
+  static defaultProps = {
+    center: { lat: 39.69654, lng: -9.0516547 },
+    zoom: 14,
+  }
+  render() {
+    return (
+      <div className="Map container">
+        <div className="google-map">
+          <GoogleMapReact
+            defaultCenter={this.props.center}
+            defaultZoom={this.props.zoom}
+          >
+            <AnyReactComponent
+              lat={39.69654}
+              lng={-9.0516547}
+            />
+          </GoogleMapReact>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Map
