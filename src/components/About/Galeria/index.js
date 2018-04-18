783701d8faa690320b6f3fe50bbb1f80aca16580
Samuel Fialho <samuel@superlativo.pt>
2018-04-18 18:24:56 +0100

    fixed bug on about page

--- src/components/About/Galeria/index.js
+++ src/components/About/Galeria/index.js
@@ -11,19 +11,19 @@ class Galeria extends Component {
       <Col xs={12} className="Galeria">
         <Row className="show-grid">
           <Col xs={12} md={6} className="large">
-            <Img sizes={Images[0].node.childImageSharp.sizes} />
+            <Img sizes={Images[1].node.childImageSharp.sizes} />
           </Col>
           <Col xs={6} md={3} className="small">
-            <Img sizes={Images[0].node.childImageSharp.sizes} />
+            <Img sizes={Images[1].node.childImageSharp.sizes} />
           </Col>
           <Col xs={6} md={3} className="small">
-            <Img sizes={Images[0].node.childImageSharp.sizes} />
+            <Img sizes={Images[1].node.childImageSharp.sizes} />
           </Col>
           <Col xs={6} md={3} className="small">
-            <Img sizes={Images[0].node.childImageSharp.sizes} />
+            <Img sizes={Images[1].node.childImageSharp.sizes} />
           </Col>
           <Col xs={6} md={3} className="small">
-            <Img sizes={Images[0].node.childImageSharp.sizes} />
+            <Img sizes={Images[1].node.childImageSharp.sizes} />
           </Col>
         </Row>
       </Col>
