ddd4f36ee52a3d523a5a07bad7ff294aa0f97301
Samuel Fialho <samuel@superlativo.pt>
2018-04-19 14:17:46 +0100

    favicons

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -57,7 +57,25 @@ class TemplateWrapper extends Component {
             { name: 'og:title', content: 'Wild Drop Surfcamp' },
             { name: 'og:url', content: 'http://www.wildropsurfcamp.com' },
             { name: 'og:description', content: 'NAZARÉ / PAREDES DA VITÓRIA - HOSTEL ACCOMMODATION SURFCAMP' },
-            { name: 'og:image', content: {OGImage} },
+            { name: 'og:image', content: '../img/ogimage.jpg' },
+            { name: 'msapplication-TileColor', content: '#EC645A' },
+            { name: 'theme-color', content: '#EC645A' },
+            { name: 'msapplication-TileImage', content: '../img/meta/ms-icon-144x144.png' },
+          ]}
+          link={[
+            { rel: 'apple-touch-icon', sizes: '57x57', href: '../img/meta/apple-icon-57x57.png' },
+            { rel: 'apple-touch-icon', sizes: '60x60', href: '../img/meta/apple-icon-60x60.png' },
+            { rel: 'apple-touch-icon', sizes: '72x72', href: '../img/meta/apple-icon-72x72.png' },
+            { rel: 'apple-touch-icon', sizes: '76x76', href: '../img/meta/apple-icon-76x76.png' },
+            { rel: 'apple-touch-icon', sizes: '114x114', href: '../img/meta/apple-icon-114x114.png' },
+            { rel: 'apple-touch-icon', sizes: '120x120', href: '../img/meta/apple-icon-120x120.png' },
+            { rel: 'apple-touch-icon', sizes: '144x144', href: '../img/meta/apple-icon-144x144.png' },
+            { rel: 'apple-touch-icon', sizes: '152x152', href: '../img/meta/apple-icon-152x152.png' },
+            { rel: 'apple-touch-icon', sizes: '180x180', href: '../img/meta/apple-icon-180x180.png' },
+            { rel: 'icon', type: 'image/png', sizes: '192x192', href: '../img/meta/android-icon-192x192.png' },
+            { rel: 'icon', type: 'image/png', sizes: '32x32', href: '../img/meta/favicon-32x32.png' },
+            { rel: 'icon', type: 'image/png', sizes: '96x96', href: '../img/meta/favicon-96x96.png' },
+            { rel: 'icon', type: 'image/png', sizes: '16x16', href: '../img/meta/favicon-16x16.png' },
           ]}
         />
         <Nav />
